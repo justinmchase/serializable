aad8a9ffeb972094b78350ff7742cc9fa6321acd
Justin Chase <justin.m.chase@gmail.com>
2023-07-23 16:00:14 -0500

    use type lib

--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
-type ToJson = { toJSON(): string }
-type SerializableRecord = { [key: string]: Serializable }
+import { Type, type } from "type/mod.ts";
+
+export type ToJson = { toJSON(): string };
+export type SerializableRecord = { [key: string]: Serializable };
 
 export type Serializable =
   | string
@@ -11,43 +13,49 @@ export type Serializable =
   | SerializableRecord
   | Serializable[];
 
-function isToJson(value: unknown): value is ToJson {
-  return typeof (value as ToJson).toJSON === 'function'
+export function isToJson(value: unknown): value is ToJson {
+  return typeof (value as ToJson).toJSON === "function";
 }
 
-// deno-lint-ignore ban-types
-function toSerializableRecord(value: object): SerializableRecord {
+export function toSerializableRecord(
+  value: Record<string, unknown>,
+): SerializableRecord {
   return Object.entries(value)
-    .reduce((l, [k,v]) => ({...l, [k]: toSerializable(v) }), {})
+    .filter(([k, v]) =>
+      type(k)[0] === Type.String && type(v)[0] !== Type.Undefined
+    )
+    .reduce((l, [k, v]) => ({ ...l, [k]: toSerializable(v) }), {});
 }
 
 export function toSerializable(value: unknown): Serializable {
-  switch (typeof value) {
-    case "number":
-    case "boolean":
-    case "string":
-      return value;
-    case "bigint":
-      return value.toString()
-    case "object": {
-      if (value === null) return null;
-      else if (Array.isArray(value)) return value.map(v => toSerializable(v));
-      else if (value instanceof Error) {
-        const { name, message, stack, cause, ...rest } = value;
-        return {
-          name,
-          message,
-          stack,
-          ...cause !== undefined ? { cause: toSerializable(cause) } : {},
-          ...toSerializableRecord(rest)
-        }
-      } else if (isToJson(value)) {
-        return value
-      } else {
-        return toSerializableRecord(value)
-      }
+  const [t, v] = type(value);
+  switch (t) {
+    case Type.Null:
+    case Type.Number:
+    case Type.Boolean:
+    case Type.String:
+      return v;
+    case Type.BigInt:
+      return v.toString();
+    case Type.Array:
+      return v.map((v) => toSerializable(v));
+    case Type.Error: {
+      const { name, message, stack, cause, ...rest } = v;
+      return {
+        name,
+        message,
+        stack,
+        ...cause !== undefined ? { cause: toSerializable(cause) } : {},
+        ...toSerializableRecord(rest),
+      };
     }
+    case Type.Object:
+      if (isToJson(value)) {
+        return value;
+      } else {
+        return toSerializableRecord(v);
+      }
     default:
-      return undefined
+      return undefined;
   }
 }
--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-import { assertEquals} from "std/testing/asserts.ts"
-import { toSerializable } from "./mod.ts"
+import { assertEquals } from "std/testing/asserts.ts";
+import { toSerializable } from "./mod.ts";
 
 class TestError extends Error {
   constructor(public readonly extra = 100) {
-    super("test error 123", { cause: { x: 1, y: 2 }})
-    this.name = "TestError"
+    super("test error 123", { cause: { x: 1, y: 2 } });
+    this.name = "TestError";
   }
 }
 
@@ -19,29 +19,33 @@ Deno.test({
       [0, 0],
       [BigInt(100), "100"],
       [[], []],
+      [[1, [2, [3]]], [1, [2, [3]]]],
       [new TestError(), {
         message: "test error 123",
         name: "TestError",
         cause: { x: 1, y: 2 },
-        extra: 100
+        extra: 100,
       }],
-      [{ toJSON: () => "[]"}, { toJSON: () => "[]"}],
+      [{ toJSON: () => "[]" }, { toJSON: () => "[]" }],
       [{ x: 1, y: true, z: "three" }, { x: 1, y: true, z: "three" }],
-      [new Date('2020-01-01T00:00:00.000Z'), new Date('2020-01-01T00:00:00.000Z')],
-    ]
+      [
+        new Date("2020-01-01T00:00:00.000Z"),
+        new Date("2020-01-01T00:00:00.000Z"),
+      ],
+    ];
 
     for (const [value, expected] of data) {
       await t.step({
         name: `${value} -> ${expected}`,
         fn: () => {
-          const actual = toSerializable(value)
-          delete (actual as unknown as { stack: unknown })?.stack
+          const actual = toSerializable(value);
+          delete (actual as unknown as { stack: unknown })?.stack;
           assertEquals(
             JSON.parse(JSON.stringify(actual) ?? "null"),
             JSON.parse(JSON.stringify(expected) ?? "null"),
-          )
-        }
-      })
+          );
+        },
+      });
     }
-  }
-})
+  },
+});
